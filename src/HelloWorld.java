5ad1dd501083173644b43ce698dc240d37ad68d3
psh12320 <pshricharan2020@gmail.com>
2024-01-21 16:24:37 +0800

    new commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,13 +1,13 @@
 public class HelloWorld {
     public static void main(String args[]) {
-        System.out.printf("Hello CS2040S! \n");
+        System.out.print("Hello CS2040S \n");
 
-        System.out.printf("My name is Shri! \n");
+        System.out.print("My name is Shri \n");
 
-        System.out.printf("A joke I read was that someone managed to steal the last few pages of the code for a missile launch written in Lisp, but they all looked like this: )))))))))))))))))) \n");
+        System.out.print("A joke I read was that someone managed to steal the last few pages of the code for a missile launch written in Lisp, but they were all just closing brackets \n");
 
-        System.out.printf("I have been pretty interested in Telegram bots and like coding all sorts of things with them. \nAdditionally, I like using many different APIs in these bots. \nAn example would be using the government API to find out the bus timings through Telegram \n");
+        System.out.print("I have been pretty interested in Telegram bots and like coding all sorts of things with them. \nAdditionally, I like using many different APIs in these bots. \nAn example would be using the government API to find out the bus timings through Telegram \n");
 
-        System.out.printf("My answer to problem 1a is 3125 and for problem 1b, it is a function calculating the power of argA to argB, where argA is the base");
+        System.out.print("My answer to problem 1a is 3125. For problem 1b, it is a function calculating the power of argA to argB, where argA is the base");
     }
 }
